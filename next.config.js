ceb1401b7d94a80ffb9236b281efed8eee10417f
Caio Anderson Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2022-12-29 13:45:34 -0300

    ➕  Config pageExtensions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Apenas arquivos que termine com essas extenções abaixo será considerado paginas.
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 }
 
 module.exports = nextConfig
